73abc0a14a871ae48249767c64a8c0213cce864f
purin <purin715@naver.com>
2021-07-12 18:19:43 +0900

    4

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from accountapp.views import young
+from accountapp.views import young, AccountCreateView
 
 app_name = 'accountapp'
 
 urlpatterns = [
-    path('young/', young, name='young')
+    path('young/', young, name='young'),
+
+    path('create/', AccountCreateView, name='create')
 ]
--- accountapp/views.py
+++ accountapp/views.py
@@ -1,8 +1,11 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView
 
 from accountapp.models import HelloWorld
 
@@ -16,9 +19,16 @@ def young(request):
         new_data.save()
 
         return HttpResponseRedirect(reverse('accountapp:young'))
+
         # data_list = HelloWorld.objects.all()
         #
         # return render(request, 'accountapp/young.html', context={'data_list' : data_list})
     else:
         data_list = HelloWorld.objects.all()
         return render(request, 'accountapp/young.html', context={'data_list' : data_list})
+
+class AccountCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = reverse_lazy('accountapp:young')
+    template_name = 'accountapp/create.html'
