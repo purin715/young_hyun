b933be550f60a700c0e3e2224dc6d3e784ab2933
purin <purin715@naver.com>
2021-07-12 17:16:32 +0900

    4

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,10 +1,24 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse
+
+from accountapp.models import HelloWorld
+
 
 def young(request):
+
     if request.method == 'POST':
-        return render(request, 'accountapp/young.html', context={'text':'POST METHOD!'})
+        temp = request.POST.get('input')
+        new_data = HelloWorld()
+        new_data.text = temp
+        new_data.save()
+
+        return HttpResponseRedirect(reverse('accountapp:young'))
+        # data_list = HelloWorld.objects.all()
+        #
+        # return render(request, 'accountapp/young.html', context={'data_list' : data_list})
     else:
-        return render(request, 'accountapp/young.html', context={'text': 'GET METHOD!'})
+        data_list = HelloWorld.objects.all()
+        return render(request, 'accountapp/young.html', context={'data_list' : data_list})
