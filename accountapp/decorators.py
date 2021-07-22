0e3cf2ce716de99e46c3b43ab5e66a2d8c94fae5
purin <purin715@naver.com>
2021-07-22 17:22:35 +0900

    1

--- accountapp/decorators.py
+++ accountapp/decorators.py
@@ -6,7 +6,7 @@ def account_ownership_required(func):
     def decorated(request, *args, **kwargs):
         target_user = User.objects.get(pk=kwargs['pk'])
         if target_user == request.user:
-            return func
+            return func(request, *args, **kwargs)
         else:
             return HttpResponseForbidden()
     return decorated
