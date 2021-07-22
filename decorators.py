41f22b621edcb82bba26fde17c3cdf47f7465ed0
purin <purin715@naver.com>
2021-07-22 17:00:17 +0900

    1

--- decorators.py
+++ decorators.py
@@ -1,15 +1,20 @@
 def decorator(func):
     def decorated(width, height):
         if width >= 0 and height >= 0:
-            return func(width, height)
+            res = func(width, height)
+            print(res)
         else:
             print("False")
 
     return decorated
 
-
+@decorator
 def tri(width, height):
     return width * height / 2
 
+@decorator
 def reg(width, height):
     return width * height
+
+tri(2, 3)
+reg(-1, 3)
