7a99792720ec0e93aa25a93d27819a308999dda1
purin <purin715@naver.com>
2021-08-09 16:56:43 +0900

    2

--- commentapp/models.py
+++ commentapp/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+from articleapp.models import Article
+
+
+class Comment(models.Model): # 일대다이므로 forienkey로 연결
+    article = models.ForeignKey(Article,
+                                on_delete=models.SET_NULL,
+                                related_name='comment',
+                                null=True)
+    writer = models.ForeignKey(User,
+                               on_delete=models.SET_NULL,
+                               related_name='comment',
+                               null=True)
+    comment = models.TextField(null=False)
+
+    created_at = models.DateTimeField(auto_now=True)
