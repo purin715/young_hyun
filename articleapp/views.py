66581cf7759af624f307646aff12e16a1f1af90e
purin <purin715@naver.com>
2021-08-09 18:00:42 +0900

    5

--- articleapp/views.py
+++ articleapp/views.py
@@ -43,6 +43,11 @@ class ArticleUpdateView(UpdateView):
     context_object_name = 'target_article'
     template_name = 'articleapp/update.html'
 
+    def form_valid(self, form):
+        form.instance.writer = self.request.user
+        form.instance.article_id = self.request.POST.get('article_pk')
+        return super().form_valid()
+
     def get_success_url(self):
         return reverse('articleapp:detail', kwargs={'pk': self.object.pk})
 
