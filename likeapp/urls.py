263fe49e8b3cd2e8a1b5fe13c69bdb8ba6f890e6
purin <purin715@naver.com>
2021-08-25 11:20:58 +0900

    1

--- likeapp/urls.py
+++ likeapp/urls.py
@@ -1,3 +1,10 @@
+from django.urls import path
+
+from likeapp.views import LikeArticleView
+
+app_name = 'likeapp'
+
 urlpatterns = [
+    path('like/<int:article_pk>', LikeArticleView.as_view(), name='article_like'),
 
 ]
