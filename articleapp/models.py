ad8691876b0e1cbed0b3f09e0a673ff56585799d
purin <purin715@naver.com>
2021-08-26 18:01:14 +0900

    4

--- articleapp/models.py
+++ articleapp/models.py
@@ -11,7 +11,7 @@ class Article(models.Model):
                                 # 어떻게 접근할 것인지 이름으로
 
     project = models.ForeignKey(Project, on_delete=models.SET_NULL,
-                                related_name='article', null=True)
+                                related_name='article', null=True, blank=True)
 
     title = models.CharField(max_length=200, null=True)
     image = models.ImageField(upload_to='article/', null=True)
