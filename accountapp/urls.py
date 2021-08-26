1cc4110d0cf535879e061389f6cac0cfc37f1cd0
purin <purin715@naver.com>
2021-08-26 17:49:53 +0900

    3

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,13 +1,11 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from accountapp.views import young, AccountCreateView, AccountDetailView, AccountUpdateView, AccountDeleteView
+from accountapp.views import AccountCreateView, AccountDetailView, AccountUpdateView, AccountDeleteView
 
 app_name = 'accountapp'
 
 urlpatterns = [
-    path('young/', young, name='young'),
-
     path('login/', LoginView.as_view(template_name = 'accountapp/login.html'), name='login'),
 
     path('logout/', LogoutView.as_view(), name='logout'),
