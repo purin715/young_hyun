0aa0ca2fcc556a1878d791354c170b5d7db4f7f7
purin715 <purin715@naver.com>
2021-09-23 17:47:42 +0900

    3

--- young_hyun/settings/local.py
+++ young_hyun/settings/local.py
@@ -19,7 +19,7 @@ while True:
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&i)0^u1x3&vl3)ko$pg+%)sqo#j7^#47zxn7is4q6^_up838b$'
+SECRET_KEY = env_list['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
